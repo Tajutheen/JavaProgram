a2f8525418a75af0801ccbbfd222f15aa20cbcbf
Tajutheen <tajutheen.info@gmail.com>
2020-08-17 16:43:51 +0530

    Javaprogram

---
+++ TestCalculator.java
@@ -0,0 +1,14 @@
+class Calculator
+{
+	public static void main(String args[])
+	{
+		int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
+	void add(i)
+	{
+		system.out.println("Done");
+	}
